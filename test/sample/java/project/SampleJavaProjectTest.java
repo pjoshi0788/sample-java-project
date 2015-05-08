4be647df229f3316dc2ac18491238bd4b32bc6c9
praveen.joshi <praveen.joshi@CLBLAP462.BRILLIO.COM>
2015-05-08 14:53:25 +0530

    Removed the unused import.

--- test/sample/java/project/SampleJavaProjectTest.java
+++ test/sample/java/project/SampleJavaProjectTest.java
@@ -1,7 +1,7 @@
 package sample.java.project;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+//import static org.junit.Assert.assertNull;
 import org.junit.Before;
 import org.junit.Test;
 
